84e8deb8a47b537f61811b5945061dcd5ed6ee71
artyom <artyom.pasyshen@do-it.co>
2017-05-06 21:53:24 +0300

    updated

---
+++ src/helpers/formComponets.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const renderField = ({ input, label, type, meta: { touched, error } }) => (
+  <div className="field-wrapper">
+    {touched && error ? (
+      <label className="error">{error}</label>
+    ) : (
+      <label>{label}</label>
+    )}
+    <div className="field-wrapper-input">
+      <input {...input} type={type} />
+    </div>
+  </div>
+);
+
+
+const renderDate = ({ input, label, type, placeholder, meta: { touched, error } }) => (
+  <div className="field-wrapper-date">
+    <input {...input} type={type} placeholder={placeholder} />
+    {touched && error && <label className="error-date">{error}</label>}
+  </div>
+);
+
+const renderSelector = ({input, meta: {touched, error}, options}) => (
+  <div>
+    <label className="special-label">WHERE DID YOU HERE ABOUT US</label>
+    <div className="styled-select slate">
+      <select {...input}>
+        <option value=""></option>
+        {options.map(val => <option value={val} key={val}>{val}</option>)}
+      </select>
+      {touched && error && <span>{error}</span>}
+    </div>
+  </div>
+);
+
+const renderError = ({meta: {touched, error}}) =>
+  touched && error ? <p className="error">{error}</p> : false;
+
+
+export {
+  renderDate,
+  renderField,
+  renderSelector,
+  renderError
+}
+
